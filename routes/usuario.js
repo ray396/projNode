4219a8e40140aefe6974eaaf1f431e0574641276
Rayssa Ellen <65548341+ray396@users.noreply.github.com>
2023-07-12 16:55:31 -0300

    sexta

---
+++ routes/usuario.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+require("../models/Usuario")
+const Usuario = mongoose.model("usuarios")
+const bcrypt = require("bcryptjs")
+
+router.get("/registros", (req, res) => {
+    res.render("usuarios/registros")
+})
+
+router.post("/registros", (req, res) => {
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
+    }
+    if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
+        erros.push({texto: "Email inválido"})
+    }
+    if(!req.body.senha || typeof req.body.senha == undefined || req.body.senha == null){
+        erros.push({texto: "Senha inválido"})
+    }
+    if(req.body.senha.length < 4){
+        erros.push({texto: "Senha muito curta"})
+    }
+    if(req.body.senha != req.body.senha2){
+        erros.push({texto: "As senha são diferentes, tente novamente!"})
+    }
+
+    if(erros.length > 0){
+        res.render("usuarios/registros", {erros: erros})
+    }else{
+        Usuario.findOne({email: req.body.email}).then((usuario) => {
+            if(usuario){
+                req.flash("error_msg", "Já existe uma conta com este e-mail no nosso sistema")
+                res.redirect("/registros")
+            }else{
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    }
+
+})
+
+module.exports = router
